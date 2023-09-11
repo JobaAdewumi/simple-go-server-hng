2eee1352026f52b64657f2e52076b4876ebfb5ea
Joba Adewumi <jobaadewumis@gmail.com>
2023-09-11 02:04:51 +0100

    Restructured code

--- api/server.go
+++ api/server.go
@@ -7,26 +7,27 @@ import (
 	"time"
 )
 
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	// Get query parameters from url
+	slack_name := r.URL.Query().Get("slack_name")
+	if slack_name == "" {
+		slack_name = "Joba Adewumi"
+	}
+	track := r.URL.Query().Get("track")
+	if track == "" {
+		track = "Backend"
+	}
+
+	// get current day and utc date
+	currentUtcTime := time.Now().UTC()
+	currentDay := time.Now().Weekday()
+
+	// Response back in json format
+	fmt.Fprintf(w, `{"slack_name": "%[3]s", "current_day": "%[1]s", "utc_time": "%[2]s", "track": "%[4]s", "github_file_url": "https", "github_repo_url": "https", "status_code": 200}`, currentDay, currentUtcTime, slack_name, track)
+}
+
 func Main() {
-
-	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		// Get query parameters from url
-		slack_name := r.URL.Query().Get("slack_name")
-		if slack_name == "" {
-			slack_name = "Joba Adewumi"
-		}
-		track := r.URL.Query().Get("track")
-		if track == "" {
-			track = "Backend"
-		}
-
-		// get current day and utc date
-		currentUtcTime := time.Now().UTC()
-		currentDay := time.Now().Weekday()
-
-		// Response back in json format
-		fmt.Fprintf(w, `{"slack_name": "%[3]s", "current_day": "%[1]s", "utc_time": "%[2]s", "track": "%[4]s", "github_file_url": "https", "github_repo_url": "https", "status_code": 200}`, currentDay, currentUtcTime, slack_name, track)
-	})
+	http.Handle("/api", http.HandlerFunc(handleFunc))
 }
 
 // Main function
